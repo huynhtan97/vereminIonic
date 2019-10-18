648fad2abfd34b7241b84c6a23afff420d27c5c9
An Huynh <huynha1@wit.edu>
2019-10-18 10:59:30 -0400

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import 'capacitor-camera-preview';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// Call the element loader after the app has been rendered the first time
+defineCustomElements(window);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,55 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
+import React, { useState } from 'react';
+import { Plugins } from "@capacitor/core"
+
+import * as posenet from '@tensorflow-models/posenet';
+import testImage from '../static_assets/testImage.jpeg';
+import "./Home.css";
+// import * as Tone from 'tone';
+
+// const playTone = () => {
+//   const synth = new Tone.Synth().toMaster();
+//   synth.triggerAttackRelease("C4", "8n");
+// }
+
+const { CameraPreview } = Plugins
+
+
+const predict = async () => {
+  const image = new Image();
+  image.src = testImage;
+  const model = await posenet.load();
+  const pose = await model.estimateSinglePose(image, { flipHorizontal: false });
+  return pose
+}
 
 const Home: React.FC = () => {
+  const [poseData, setPoseData] = useState({});
+  const [error, setError] = useState("");
+  const startCamera = async () => {
+    await CameraPreview.start({ parent: "bigboi", position: "rear", className: "camera" });
+  }
+  console.log("here");
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
+          <IonTitle>Veremin</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
+        <IonContent style={{backgroundColor: "transparent"}} id="bigboi" />
+        {JSON.stringify(poseData)}
+        <IonTitle>{error}</IonTitle>
+        <IonButton onClick={async () => {
+          try {
+            await startCamera()
+            setPoseData(await predict())
+          } catch (err) {
+            setError(JSON.stringify(err));
+            console.error(err);
+          }
+        }}>Play music</IonButton>
       </IonContent>
     </IonPage>
   );
